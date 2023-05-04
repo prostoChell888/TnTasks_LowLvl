f0cf8363265d9c8fa335927e2900ab8415d7e254
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-04 14:21:42 +0300

    add check for null and empty str

--- src/main/java/ru/tn/PalindromeChecker.java
+++ src/main/java/ru/tn/PalindromeChecker.java
@@ -3,6 +3,9 @@ package ru.tn;
 public class PalindromeChecker {
 
     public static boolean isPalindrome(String str) {
+        if(str == null) return false;
+        if (str.length() < 2 ) return true;
+
         str = str.toLowerCase();
         for (int i= 0; i < str.length() / 2; i++){
             if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
