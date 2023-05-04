8cea06d0cea3d99a194b771a4194131262bba993
Александр Бахмутов <alexdmitralex@gmail.com>
2023-05-04 14:36:07 +0300

    add foo to check num is palindrome

--- src/main/java/ru/tn/PalindromeChecker.java
+++ src/main/java/ru/tn/PalindromeChecker.java
@@ -3,15 +3,28 @@ package ru.tn;
 public class PalindromeChecker {
 
     public static boolean isPalindrome(String str) {
-        if(str == null) return false;
-        if (str.length() < 2 ) return true;
+        if (str == null) return false;
+        if (str.length() < 2) return true;
 
         str = str.toLowerCase();
-        for (int i= 0; i < str.length() / 2; i++){
+        for (int i = 0; i < str.length() / 2; i++) {
             if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
                 return false;
             }
         }
         return true;
     }
+
+    public static boolean isPalindrome(int num) {
+        int  reverseNum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            num /= 10;
+            reverseNum = reverseNum  * 10 + digit;
+        }
+
+        return reverseNum == num;
+    }
+
 }
