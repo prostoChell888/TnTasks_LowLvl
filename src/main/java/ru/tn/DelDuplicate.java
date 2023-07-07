1e43ca22962c1c2ca52111f64c6ffc5f31a7a541
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-07 17:21:21 +0300

    change algorithm of removeDuplicates

--- src/main/java/ru/tn/DelDuplicate.java
+++ src/main/java/ru/tn/DelDuplicate.java
@@ -1,26 +1,69 @@
 package ru.tn;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 
 public class DelDuplicate {
-    public static <T extends Number> T[] removeDuplicates(T[] arr) {
-        int uniqueCount = 0;
-        for (int i = 0; i < arr.length; i++) {
-            boolean isDuplicate = false;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i].equals(arr[j])) {
-                    isDuplicate = true;
-                    break;
-                }
+
+    static class Pair<T extends Comparable<T>> {
+        Integer position;
+        T element;
+
+        public Pair(int position, T element) {
+            this.position = position;
+            this.element = element;
+        }
+    }
+
+
+    public static <T extends Comparable<T>> T[] removeDuplicates(T[] arr) {
+        var newArr = Arrays.copyOf(arr, arr.length);
+        if (newArr.length < 2) return newArr;
+
+        Pair<T>[] elementsWithSavedPositions = savePos(newArr);
+        Pair<T>[] sortedArrWithUniqueElements = sortArrAndDelDuplicates(elementsWithSavedPositions);
+        getArrWithOriginPos(newArr, sortedArrWithUniqueElements);
+
+        return Arrays.copyOf(newArr, sortedArrWithUniqueElements.length);
+    }
+
+    private static <T extends Comparable<T>> void getArrWithOriginPos(T[] newArr, Pair<T>[] sortedArrWithUniqueElements) {
+        Arrays.sort(sortedArrWithUniqueElements, Comparator.comparing(o -> o.position));
+        for (int i = 0; i < sortedArrWithUniqueElements.length; i++) {
+            newArr[i] = sortedArrWithUniqueElements[i].element;
+        }
+    }
+
+    private static <T extends Comparable<T>> Pair<T>[] sortArrAndDelDuplicates(Pair<T>[] elementsWithSavedPositions) {
+        Arrays.sort(elementsWithSavedPositions, (o1, o2) -> {
+            var resOfCompElComp = o1.element.compareTo(o2.element);
+            if (resOfCompElComp == 0) {
+                return o1.position.compareTo(o2.position);
             }
-            if (!isDuplicate) {
-                arr[uniqueCount++] = arr[i];
+            return resOfCompElComp;
+        });
+        int posOfNewArr = 0;
+
+        return dellDuplicateInSortedArr(elementsWithSavedPositions, posOfNewArr);
+    }
+
+    private static <T extends Comparable<T>> Pair<T>[] dellDuplicateInSortedArr(Pair<T>[] elementsWithSavedPositions, int posOfNewArr) {
+        for (int i = 0; i < elementsWithSavedPositions.length - 1; i++) {
+            if (!elementsWithSavedPositions[i].element.equals(elementsWithSavedPositions[i + 1].element)) {
+                elementsWithSavedPositions[posOfNewArr] = elementsWithSavedPositions[i];
+                posOfNewArr++;
             }
         }
-        return Arrays.copyOf(arr, uniqueCount);
+        elementsWithSavedPositions[posOfNewArr++] = elementsWithSavedPositions[elementsWithSavedPositions.length - 1];
+
+        return Arrays.copyOf(elementsWithSavedPositions, posOfNewArr);
+    }
+
+    private static <T extends Comparable<T>> Pair<T>[] savePos(T[] arr) {
+        Pair<T>[] elementsWithSavedPositions = new Pair[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            elementsWithSavedPositions[i] = new Pair<T>(i, arr[i]);
+        }
+        return elementsWithSavedPositions;
     }
 }
