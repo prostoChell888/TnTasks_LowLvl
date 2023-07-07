a03b72a55e0bd0a4c24e1348ef1c1d609d88a00b
Александр Бахмутов <alexdmitralex@gmail.com>
2023-07-07 17:35:20 +0300

    save changes

--- src/test/java/ru/tn/PalindromeCheckerTest.java
+++ src/test/java/ru/tn/PalindromeCheckerTest.java
@@ -2,10 +2,10 @@ package ru.tn;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PalindromeCheckerTest {
 
